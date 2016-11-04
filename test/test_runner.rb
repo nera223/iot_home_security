9296fccea7cc27addcd7a69f6ae0b74838561acf
omwe <ole.wendroth@gmail.com>
2016-11-04 03:04:07 +0000

    optimized testing and requires

--- test/test_runner.rb
+++
@@ -1,9 +0,0 @@
-# To run mini tests
-require 'minitest/autorun'
-# To load the framework files
-require_relative '../lib/framework' # framework.rb requires other files
-# Load test files
-Dir[File.expand_path('../**/*_test.rb')].each do |f|
-	load f
-end
-
