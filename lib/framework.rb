a776001b2870cab158df9c381dfcf66040745f11
omwe <ole.wendroth@gmail.com>
2016-11-04 03:49:06 +0000

    added nice framework for server begin

--- lib/framework.rb
+++ lib/framework.rb
@@ -4,4 +4,6 @@ $LOAD_PATH << File.expand_path('..', __FILE__)
 
 module Framework
 	autoload :VERSION, 'framework/version'
+	autoload :WEBrickServer, 'framework/servers/webrick_server'
+	autoload :AbstractServer, 'framework/servers/abstract_server'
 end
---
+++ lib/framework/servers/webrick_server.rb
@@ -0,0 +1,25 @@
+require 'webrick'
+
+module Framework
+	class WEBrickServer < Framework::AbstractServer
+		def log(message, level=:info)
+			http_server.logger.public_send(level, message)
+		end
+
+		private
+
+		def start_server
+			http_server.start
+		end
+
+		def stop_server
+			http_server.stop
+			@http_server = nil
+		end
+	
+		def http_server
+			@http_server ||= WEBrick::HTTPServer.new(configuration)
+		end
+	
+	end # WEBrickServer
+end # Framework
